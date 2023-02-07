f7f1a0203b72bab572f0c2d491dd78b32c25e2f0
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2023-02-07 11:33:48 +0300

    added DOM Elements with Object Oriented Programming

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+console.log("Hey there!")
+
+class ProjectInput {
+  tempEl: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.tempEl = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostEl = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.tempEl.content, true); // take the content of the template.
+    this.element = importedNode.firstElementChild as HTMLFormElement; // put the content of the importNode inside the host which is div 'app'.
+
+    this.attach();
+  }
+
+  private attach() {
+    this.hostEl.insertAdjacentElement("afterbegin", this.element); // insert the form element inside the host.
+  }
+}
+
+const prjInput = new ProjectInput();
