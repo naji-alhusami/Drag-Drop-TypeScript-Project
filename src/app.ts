73dac01dbfbb612f4aeaef04703ce5f7524a84a1
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2023-02-07 13:29:11 +0300

    Added Decorater for binding (this)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
-console.log("Hey there!")
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 
 class ProjectInput {
   tempEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
   element: HTMLFormElement;
+  titleInput: HTMLInputElement;
+  descInput: HTMLInputElement;
+  peopleInput: HTMLInputElement;
 
   constructor() {
     this.tempEl = document.getElementById(
@@ -13,8 +26,28 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.tempEl.content, true); // take the content of the template.
     this.element = importedNode.firstElementChild as HTMLFormElement; // put the content of the importNode inside the host which is div 'app'.
+    this.element.id = "user-input";
 
+    this.titleInput = this.element.querySelector("#title") as HTMLInputElement;
+    this.descInput = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInput = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.config();
     this.attach();
+  }
+
+  @AutoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInput.value);
+  }
+
+  private config() {
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
