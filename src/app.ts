70917dc6f4d4981c6b76d08b7e26336fa3972a74
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2023-02-08 12:18:46 +0300

    Added custom alerts validations

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,79 @@
 interface Validation {
-    value: string | number;
-    required?: boolean; // "?" optional OR boolean | undefined
-    minLen?: number; // check the length of the string
-    maxLen?: number;
-    min?: number; // check the value of the number
-    max?: number;
+  // input: HTMLInputElement;
+  inputName: string;
+
+  value: string | number;
+  required?: boolean; // "?" optional OR boolean | undefined
+  minLen?: number; // check the length of the string
+  maxLen?: number;
+  min?: number; // check the value of the number
+  max?: number;
 }
 
-function validate(inputValidation: Validation) {
+function validate(inputValidation: Validation): [boolean, string[]] {
   let isValid = true;
+  let errors: string[] = []; // alert type
+
   if (inputValidation.required) {
     isValid = isValid && inputValidation.value.toString().trim().length !== 0;
+    if (!(inputValidation.value.toString().trim().length !== 0)) {
+      errors.push(`the property ${inputValidation.inputName} must be present`);
+    }
   }
   if (
     inputValidation.minLen != null &&
     typeof inputValidation.value === "string"
   ) {
     isValid = isValid && inputValidation.value.length >= inputValidation.minLen;
+    if (!(inputValidation.value.length >= inputValidation.minLen)) {
+      errors.push(
+        `the property ${inputValidation.inputName} must have a length greater than ${inputValidation.minLen}`
+      );
+    }
   }
   if (
     inputValidation.maxLen != null &&
     typeof inputValidation.value === "string"
   ) {
     isValid = isValid && inputValidation.value.length <= inputValidation.maxLen;
+    if (!(inputValidation.value.length <= inputValidation.maxLen)) {
+      errors.push(
+        `the property ${inputValidation.inputName} must have a length lower or equal to ${inputValidation.maxLen}`
+      );
+    }
   }
   if (
     inputValidation.min != null &&
     typeof inputValidation.value === "number"
   ) {
     isValid = isValid && inputValidation.value >= inputValidation.min;
+    if (!(inputValidation.value >= inputValidation.min)) {
+      errors.push(
+        `the property ${inputValidation.inputName} must have a value greater than ${inputValidation.min}`
+      );
+    }
   }
   if (
     inputValidation.max != null &&
     typeof inputValidation.value === "number"
   ) {
     isValid = isValid && inputValidation.value <= inputValidation.max;
+    if (!(inputValidation.value <= inputValidation.max)) {
+      errors.push(
+        `the property ${inputValidation.inputName} must have a value lower or equal to ${inputValidation.max}`
+      );
+    }
   }
-  return isValid;
+  return [isValid, errors];
+}
+
+function showCustomAlert(errorMessages: string[]): void {
+  let customMessage: string = "";
+
+  if (errorMessages.length > 0) {
+    customMessage = `${errorMessages}.`;
+    alert(customMessage);
+  }
 }
 
 function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
@@ -86,17 +123,43 @@ class ProjectInput {
     const enteredDesc = this.descInput.value;
     const enteredPeople = this.peopleInput.value;
 
-    if (
-      !validate({ value: enteredTitle, required: true }) ||
-      !validate({
-        value: enteredDesc,
-        required: true,
-        minLen: 5,
-        maxLen: 20,
-      }) ||
-      !validate({ value: +enteredPeople, required: true, min: 1, max: 10 })
-    ) {
-      alert("Please Try Again With Validatable Values");
+    const titleValidation: Validation = {
+      inputName: "title",
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descValidation: Validation = {
+      inputName: "description",
+      value: enteredDesc,
+      required: true,
+      minLen: 5,
+      maxLen: 20,
+    };
+
+    const peopleValidation: Validation = {
+      inputName: "people",
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 10,
+    };
+
+    const [titleIsValid, titleErrors]: [boolean, string[]] =
+      validate(titleValidation);
+    const [descriptionIsValid, descriptionErrors]: [boolean, string[]] =
+      validate(descValidation);
+    const [peopleAmountIsValid, peopleAmountErrors]: [boolean, string[]] =
+      validate(peopleValidation);
+
+    // if(!titleIsValid) {
+    //     showCustomAlert (titleErrors);
+    // }
+
+    if (!titleIsValid || !descriptionIsValid || !peopleAmountIsValid) {
+      showCustomAlert(
+        titleErrors.concat(descriptionErrors).concat(peopleAmountErrors)
+      );
       return;
     } else {
       return [enteredTitle, enteredDesc, +enteredPeople];
@@ -110,9 +173,9 @@ class ProjectInput {
     if (Array.isArray(inputs)) {
       const [title, description, people] = inputs;
       console.log(title, description, people);
-      this.titleInput.value = '';
-      this.descInput.value = '';
-      this.peopleInput.value = '';
+      this.titleInput.value = "";
+      this.descInput.value = "";
+      this.peopleInput.value = "";
     }
   }
 
